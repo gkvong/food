3d604169030f397112f789ff0c538d4c368314c1
Garry Vong <gvong@outlook.com>
2023-05-23 17:40:14 +1000

    added restaurant update functionality

--- app.py
+++ app.py
@@ -18,22 +18,33 @@ def get_previous_monday():
 def get_current_week():
     return datetime.date.today().isocalendar()[1]
 
+def get_recent_restaurants():
+    with open('recent_restaurants.txt', 'r') as file:
+        recent_restaurants = [line.strip().split(' - ')[0] for line in file.readlines() if line.strip()]
+
+    return recent_restaurants
+
 def get_restaurant_type(current_week):
-    restaurant_types = ['Chinese', 'Korean', 'Japanese', 'Mexican (GYG)', 'Indian', 'Vietnamese', 'Thai']
-    selected_types = ['Chinese', 'Indian']  # Sample data, update as needed
-    
-    last_index = (current_week - 1) % len(restaurant_types)
-    
+    restaurant_types = ['Chinese', 'Korean', 'Japanese', 'Mexican', 'Indian', 'Vietnamese', 'Thai']
+    selected_types = get_recent_restaurants()  # Load recent restaurants from file
+
     if len(selected_types) >= len(restaurant_types):
-        selected_types = []
-    
-    while True:
-        restaurant_type = random.choice(restaurant_types)
-        if restaurant_type not in selected_types:
-            break
-    
+        # Reset the list of recent restaurants
+        last_entry = selected_types[-1]  # Remember the last entry
+        selected_types = [last_entry]  # Reset the selected types with the last entry
+        print("Cycle Restart.")
+
+    available_restaurants = [restaurant for restaurant in restaurant_types if restaurant not in selected_types]
+
+    restaurant_type = random.choice(available_restaurants)
     selected_types.append(restaurant_type)
-    
+
+    # Update the recent restaurants file
+    with open('recent_restaurants.txt', 'w') as file:
+        last_update_datetime = datetime.datetime.now().strftime("%d/%m/%y %I:%M %p")
+        for restaurant in selected_types:
+            file.write(f"{restaurant} - {last_update_datetime}\n")
+
     return restaurant_type, selected_types[-2] if len(selected_types) >= 2 else None
 
 @app.route('/')
